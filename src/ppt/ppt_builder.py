6cc98cad987b723448640f831dee30dab406ea2d
JayPatel2574 <onlystudy1676@gmail.com>
2026-01-21 00:53:55 +0530

    fourth try

--- src/ppt/ppt_builder.py
+++ src/ppt/ppt_builder.py
@@ -5,7 +5,7 @@ from src.ppt.slide_types.overview import build_overview
 from src.ppt.slide_types.highlights import build_highlights
 from src.ppt.slide_types.financials import build_financials
 from src.ppt.slide_types.disclaimer import build_disclaimer
-
+from src.ai.investment_narrator import InvestmentNarrator
 
 class PPTBuilder:
 
@@ -17,13 +17,17 @@ class PPTBuilder:
         slide = self.prs.slides.add_slide(self.prs.slide_layouts[6])
         build_cover(self.prs, slide, sector)
 
+        narrator = InvestmentNarrator()
+
         # Overview
         slide = self.prs.slides.add_slide(self.prs.slide_layouts[6])
-        build_overview(self.prs, slide, data)
+        overview_text = narrator.generate_overview(data, sector)
+        build_overview(self.prs, slide, data, overview_text)
 
         # Investment Highlights
         slide = self.prs.slides.add_slide(self.prs.slide_layouts[6])
-        build_highlights(self.prs, slide, insights)
+        highlights_ai = narrator.generate_highlights(data, sector)
+        build_highlights(self.prs, slide, highlights_ai)
 
         # Financial Summary
         slide = self.prs.slides.add_slide(self.prs.slide_layouts[6])
